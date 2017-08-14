ed8e3d47da2ffe3a49be74a8fe4fd8be3177c891
Amanda Koster <amandakoster@gmail.com>
2017-08-14 11:44:15 -0700

    built out preliminary nav, may end up in app

---
+++ src/component/landing/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {connect} from 'react-redux'
+// import * as querystring from 'querystring'
+
+
+export class Landing extends React.Component{
+  render(){
+    <div className='nav'>
+
+    </div>
+
+  }
+}
+
+
+export default Landing
