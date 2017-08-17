d3fffa9c8152f4bac1622ad695f48032078f9d46
Justin Green <justin98366@gmail.com>
2017-08-17 16:14:09 -0700

    cook form tests

---
+++ src/__test__/cook-form.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+import CookForm from '../component/cook-form'
+import moment from 'moment'
+
+describe('CookForm', () => {
+  test('intial state with out props', () => {
+    let wrapper = mount(<CookForm />)
+    expect(wrapper.state('signatureDishes')).toBe('')
+    expect(wrapper.state('restaurantsCookedIn')).toBe(0)
+    expect(wrapper.state('bestDescribes')).toBe('')
+    expect(wrapper.state('mealsPerWeek')).toBe(0)
+    expect(wrapper.state('services')).toBe('')
+    expect(wrapper.state('cuisines')).toBe('')
+    expect(wrapper.state('offerDelivery')).toBe(false)
+    expect(wrapper.state('community')).toBe('')
+    expect(wrapper.state('hoursPerWeek')).toBe('')
+    expect(wrapper.state('moreInfo')).toBe('')
+    expect(wrapper.state('howDidYouHear')).toBe('')
+    // expect(wrapper.state('price')).toBe('')
+    
+  })
+})
